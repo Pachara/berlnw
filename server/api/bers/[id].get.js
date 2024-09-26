2ae159d20f757efe37f2285f71e299b345408ceb
Peter Nohng <7.microsoft@gmail.com>
2024-09-26 20:36:14 +0700

    make it better

--- server/api/bers/[id].get.js
+++ server/api/bers/[id].get.js
@@ -1,15 +1,13 @@
 export default defineEventHandler(async (event) => {
-  // Get the `id` parameter from the URL (this is the sum passed to the API)
   const { id } = event.context.params;
 
   try {
-    // Fetch the JSON file from the public directory
+    // Fetch the JSON file from the correct path (it's served as a static file)
     const data = await $fetch('/ber.json');
-  
+
     // Find the result by the sum (id)
     const result = data.results[id];
-  
-    // If result is found, return it, else return a 404 error
+
     if (result) {
       return {
         status: 'success',
